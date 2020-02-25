9ea85d3090de9c5f3eb353661ae41a72489f3395
David Gonzalez <oneproductions12@gmail.com>
2020-02-24 21:54:13 -0800

    Logger Error Bug

--- backEndCode/serverCode/Controllers/ValuesController.cs
+++ backEndCode/serverCode/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace serverCode.Controllers
@@ -10,10 +11,20 @@ namespace serverCode.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly ILoggerManager _logger;
+        public ValuesController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
+            _logger.LogInfo("Here is info message from the controller.");
+            _logger.LogDebug("Here is debug message from the controller.");
+            _logger.LogWarn("Here is warn message from the controller.");
+            _logger.LogError("Here is error message from the controller.");
+
             return new string[] { "value1", "value2" };
         }
 
