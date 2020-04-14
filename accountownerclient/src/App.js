4df9731e10480f66124029b7238f3407b47a45b1
David Gonzalez <oneproductions12@gmail.com>
2020-04-13 19:25:32 -0700

    Add navigation and 404 components

--- accountownerclient/src/App.js
+++ accountownerclient/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NotFound from './components/ErrorPages/NotFound/NotFound';
 
 function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
   );
 }
 
