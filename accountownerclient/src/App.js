b9682a721b9ec580fb051ed18195847322c07dbd
David Gonzalez <oneproductions12@gmail.com>
2020-04-16 18:49:57 -0700

    Add delete owner

--- accountownerclient/src/App.js
+++ accountownerclient/src/App.js
@@ -9,6 +9,7 @@ import InternalServer from './components/ErrorPages/InternalServer/InternalServe
 import OwnerDetails from './containers/Owner/OwnerDetails/OwnerDetails';
 import CreateOwner from './containers/Owner/CreateOwner/CreateOwner';
 import UpdateOwner from './containers/Owner/UpdateOwner/UpdateOwner';
+import DeleteOwner from './containers/Owner/DeleteOwner/DeleteOwner';
 
 const AsyncOwnerList = asyncComponent(() => {
   return import('./containers/Owner/OwnerList/OwnerList');
@@ -24,6 +25,7 @@ function App() {
             <Route path="/ownerDetails/:id" component={OwnerDetails} />
             <Route path="/createOwner" component={CreateOwner} />
             <Route path="/updateOwner/:id" component={UpdateOwner} />
+            <Route path="/deleteOwner/:id" component={DeleteOwner} />
             <Route path="/500" component={InternalServer} />
             <Route path="*" component={NotFound} />
           </Switch>
